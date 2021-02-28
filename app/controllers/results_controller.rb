ec6ffa1c882c06219d6114a556fe51abd252e1f5
Mako0530 <zhongtiancheng79@gmail.com>
2021-02-28 14:06:00 +0900

    results controller views

---
+++ app/controllers/results_controller.rb
@@ -0,0 +1,21 @@
+class ResultsController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+  
+  def destroy
+  end
+end
